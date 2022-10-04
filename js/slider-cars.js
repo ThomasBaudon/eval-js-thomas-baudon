9fe5f12acc7741c63b0d92932938f73edeefe247
ibelieveolive <thomas@thomasbaudon.fr>
2022-10-04 15:58:58 +0200

    "_"

--- js/slider-cars.js
+++ js/slider-cars.js
@@ -1,34 +1,45 @@
-const slidesContainer = document.querySelectorAll(".slides-container");
-const slide = document.querySelector(".slide");
-const previous_Button = document.querySelector(".slide-arrow-prev");
-const next_Button = document.querySelector(".slide-arrow-next");
+// const containers = document.querySelectorAll(".car-slider-container");
+// const slidesContainer = document.querySelectorAll(".slides-container");
+// const slide = document.querySelector(".slide");
+// const previous_Button = document.querySelector(".slide-arrow-prev");
+// const next_Button = document.querySelector(".slide-arrow-next");
+
+
+window.onclick = function(e) {
+    if (e.target.classList.contains("car-slider-container")){
+
+        const slide = document.querySelector(".slide");
+        console.log(slideWidth, ' left');
+
+
+        nextButton.addEventListener("click", (event) => {
+            const slideWidth = slide.clientWidth;
+              slidesContainer.scrollLeft += slideWidth;
+              
+          });
+
+    }
+
+}
 
 
 
-// window.onclick = function(e) {}
 
-slidesContainer.forEach(slideContainer => {
-    console.log(slideContainer);
+// // window.onclick = function(e) {}
 
-    // previous_Button.addEventListener('click', (e) => {
-    //      // e.target.classList.toggle("envers");
-    //      console.log('click gauche');
+// containers.forEach(container => {
 
-    //      const slideWidth = slide.clientWidth;
-    //      slideContainer.scrollLeft -= slideWidth;
-    // })
+//     const slidesContainer = document.querySelectorAll(".slides-container");
+//     const slide = document.querySelector(".slide");
 
-       
-
+//     for(let slideContainer of slidesContainer) {
+//         previous_Button.addEventListener('click', (e) => {
+//             // e.target.classList.toggle("envers");
+//             console.log('click gauche');
    
+//             const slideWidth = slide.clientWidth;
+//             slideContainer.scrollLeft -= slideWidth;
+//         });
+//     }
 
-    // if (e.target.classList.contains("slide-arrow-next")){
-
-    //     // e.target.classList.toggle("envers");
-    //     console.log('click droite');
-
-    //     const slideWidth = slide.clientWidth;
-    //     slideContainer.scrollLeft += slideWidth;
-
-    // }
-})
+//     });
